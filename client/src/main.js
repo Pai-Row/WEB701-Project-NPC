8af6a60dd447bdb2390c117394f17169e9eb48b4
Jonathan Macgregor <52035222+DualWielder@users.noreply.github.com>
2020-06-15 17:48:31 +1200

    Update main.js

--- client/src/main.js
+++ client/src/main.js
@@ -3,7 +3,7 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import Vuetify from './vuetify'
+import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
 
 Vue.config.productionTip = false
