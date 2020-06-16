0af396d446124bded4bca067d9f299ae77075cc3
Jono <jonomacgregor@gmail.com>
2020-06-16 14:51:35 +1200

    Trying to solve email exists

--- server/src/controllers/AuthenticationController.js
+++ server/src/controllers/AuthenticationController.js
@@ -1,10 +1,23 @@
 const {User} = require('../models')
+const jwt = require('jsonwebtoken')
+const config = require('../config/config')
+
+function jwtSignUser (user) {
+    const ONE_WEEK = 60 * 60 * 24 * 7
+    return jwt.sign(user, config.authentication.jwtSecret, {
+        expiresIn: ONE_WEEK
+    })
+}
 
 module.exports = {
     async register (req, res) {
         try {
             const user = await User.create(req.body)
-            res.send(user.toJSON())
+            const userJson = user.toJSON()
+            res.send({
+                user: userJson,
+                token: jwtSignUser(userJson)
+            })
         } catch (err) {
             res.status(400).send({
                 error: 'This email account is already in use.'   
@@ -26,15 +39,17 @@ module.exports = {
                     })
                 }
 
-                const isPasswordValid = password === user.password
+                const isPasswordValid = await user.comparePassword(password)
                 if (!isPasswordValid) {
                     return res.status(403).send({
                         error: 'The login information was incorrect'
                     })
                 }
+
                 const userJson = user.toJSON()
-            res.send({
-                user: userJson
+                res.send({
+                    user: userJson,
+                token:jwtSignUser(userJson)
         })
 
         } catch (err) {
--- server/src/models/User.js
+++ server/src/models/User.js
@@ -1,9 +1,40 @@
-module.exports = (sequelize, DataTypes) =>
- sequelize.define('User', {
+const Promise = require('bluebird')
+const bcrypt = Promise.promisifyAll(require('bcrypt-nodejs'))
+
+ function hashPassword (user, options) {
+    const SALT_FACTOR = 8
+
+    if (!user.chaned('password')) {
+        return;
+    }
+
+    return bcrypt
+    .genSaltAsync(SALT_FACTOR)
+    .then(salt => bcrypt.hashAsync(user.password, salt, null))
+    .then(hash => {
+        user.setDataValue('password', hash)
+    })
+}
+
+module.exports = (sequelize, DataTypes) => {
+
+ const User = sequelize.define('User', {
     email: {
         type: DataTypes.STRING,
         unique: true
     },
     password: DataTypes.STRING
+    }, {
+        hooks: {
+            beforeCreate: hashPassword,
+            beforeUpdate: hashPassword,
+            beforeSave: hashPassword
+        }
     })
 
+    User.prototype.comparePassword = function(password) {
+        return bcrypt.compareAsync(password, this.password)
+    }
+
+    return User
+}
